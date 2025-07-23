51f1a0240f7c0d040d7eb63b8cf40707605fc646
rahulkishorparida <rahulkishor943@gmail.com>
2025-07-23 12:31:34 +0530

    initial commit a-1

---
+++ src/main/java/com/sc/demo/controller/SchoolController.java
@@ -0,0 +1,18 @@
+package com.sc.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class SchoolController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "home";
+	}
+
+}
---
+++ src/test/java/com/sc/demo/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
